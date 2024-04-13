dcf1df7faefae548a1937c568b7e0fc22824aa19
Arthur <arthur.morelon@gmail.com>
2024-04-13 22:04:12 +0200

    Add LoginActivity

--- app/src/main/java/com/example/atdapp/LoginActivity.kt
+++ app/src/main/java/com/example/atdapp/LoginActivity.kt
@@ -1,32 +1,20 @@
 package com.example.atdapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // Utilisation d'un Handler pour exécuter un bloc de code après un délai de 3 secondes
-        Handler(Looper.getMainLooper()).postDelayed({
-            // Lance l'intention pour démarrer la deuxième activité
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            // Ferme l'activité actuelle
-            finish()
-        }, 3000) // 3000 millisecondes = 3 secondes
     }
 }
