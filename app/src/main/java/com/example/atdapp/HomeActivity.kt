dc5f4559d48bb4cbb5ba8c8b7a86c4faab48b6a9
Arthur <arthur.morelon@gmail.com>
2024-04-14 22:13:24 +0200

    add fragments

--- app/src/main/java/com/example/atdapp/HomeActivity.kt
+++ app/src/main/java/com/example/atdapp/HomeActivity.kt
@@ -1,10 +1,14 @@
 package com.example.atdapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,29 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        replaceFragment(HomeFragment())
+        var menu = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        menu.setOnItemSelectedListener{
+
+            when(it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.profile -> replaceFragment(ProfilFragment())
+                R.id.setting -> replaceFragment(SettingFragment())
+
+                else -> {}
+            }
+            true
+        }
     }
+
+
+    private fun replaceFragment(fragment:Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+
 }
