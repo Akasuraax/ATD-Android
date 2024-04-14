244eb27b5d5d68255a42e223fa79b2591a95847f
Arthur <arthur.morelon@gmail.com>
2024-04-14 21:10:40 +0200

    add menu bar

---
+++ app/src/main/java/com/example/atdapp/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.atdapp
+
+object Constant {
+    const val API_BASE_URL = "http://127.0.0.1:8000/api"
+}
--- app/src/main/java/com/example/atdapp/LoginActivity.kt
+++ app/src/main/java/com/example/atdapp/LoginActivity.kt
@@ -1,10 +1,19 @@
 package com.example.atdapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.SyncStateContract
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +24,36 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.emailLogin).text.toString()
+            val password = findViewById<EditText>(R.id.passwordLogin).text.toString()
+
+            if(email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(applicationContext, "valeur invalide", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            val i = Intent(this,HomeActivity::class.java)
+            startActivity(i)
+
+            val queue = Volley.newRequestQueue(this)
+            val request = StringRequest(Request.Method.GET,
+                "${Constant.API_BASE_URL}/login",
+                { content ->
+                val global = JSONObject(content)
+                    Toast.makeText(applicationContext,global.toString(),Toast.LENGTH_LONG).show()
+                },
+                { error ->
+                    Toast.makeText(applicationContext,error.message.toString(),Toast.LENGTH_LONG).show()
+                }
+            )
+            queue.add(request)
         }
     }
 }
