fd97310b124d06492257429ac5a3f7dfc5e97af6
Arthur <arthur.morelon@gmail.com>
2024-04-29 16:01:43 +0200

    En claquettes dans la zone, j'fais chanter tout l'Vélodrome

--- app/src/main/java/com/example/atdapp/HomeFragment.kt
+++ app/src/main/java/com/example/atdapp/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.atdapp
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,5 +28,33 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var button = view.findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            showSuccessDialog()
+        }
+    }
+
+    private fun showSuccessDialog() {
+
+        val successConstraintLayout = view?.findViewById<ConstraintLayout>(R.id.successConstraintLayout)
+        val inflater = LayoutInflater.from(requireActivity())
+        val dialogView = inflater.inflate(R.layout.sucess_dialog, successConstraintLayout, false)
+        val successDone = dialogView.findViewById<Button>(R.id.successDone)
+
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setView(dialogView)
+        val alertDialog = builder.create()
+
+        successDone.setOnClickListener {
+            alertDialog.dismiss()
+        }
+
+        alertDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        alertDialog.show()
     }
 }
